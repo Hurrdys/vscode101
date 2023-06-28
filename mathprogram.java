8397446a3d4be07d494c01068ac1c88c47d1bd91
Hurrdys <137699265+Hurrdys@users.noreply.github.com>
2023-06-28 12:40:53 -0700

    ihgyfdrs

---
+++ mathprogram.java
@@ -0,0 +1,48 @@
+import java.lang.Math.*;
+import java.util.Scanner;
+
+public class mathprogram {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("A constructioner named paul is going to build a simple house. He is having trouble with the shape of the roof.");
+        System.out.println("He wants to know the dimesions of the triangler roof, but cant come up with the rigth height, and base lenght.");
+        System.out.println("In order to build the roof, he needs to find the area of materails the roof will need.");
+        System.out.println("He needs your help to come up with the dimesions of the roof. ");
+       
+        System.out.println("Input a number for the height of the roof ");
+        int heigth = scanner.nextInt();
+        
+        System.out.println("Input a number for the base of the roof ");
+        int base = scanner.nextInt();
+
+        double area = 0.5 * base * heigth;
+        System.out.println("The area of of triangle will be " + area);
+
+        
+        
+        
+        
+        
+        
+        
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Enter a numebr");
+        int number  = scanner.nextInt();
+
+        
+
+
+        
+    
+        
+        
+        
+
+        }
+    }   
+        
+
+
+      
